c18e3c4188ad755ccddf29d912a716b70759dcd5
ImpossibleAmigo <cheberiako2016@gmail.com>
2026-04-07 09:54:18 +0300

    feat: add currency converter function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
-﻿print("Hello, World! Це моя перша практична робота з Git.")
+﻿def uah_to_usd(uah_amount, rate=39.5):
+    return uah_amount / rate
+
+print("Hello, World! Це моя перша практична робота з Git.")
