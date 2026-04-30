e888e0fd263135c21b34efa676b9926404eeada0
ImpossibleAmigo <cheberiako2016@gmail.com>
2026-04-30 18:12:55 +0300

    Practical 2 complete

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-﻿def uah_to_usd(uah_amount, rate=39.5):
+﻿def print_hi(name):
+    # Ця функція виводить привітання, як вимагає завдання
+    print(f'Hi, {name}')
+
+def uah_to_usd(uah_amount, rate=39.5):
     return uah_amount / rate
 
-print("Hello, World! Це моя перша практична робота з Git.")
+if __name__ == '__main__':
+    # ОСНОВНА ЧАСТИНА ЗАВДАННЯ:
+    print_hi('Олексій Чеберяко')
 
-amount = 1000
-usd_result = uah_to_usd(amount)
-print(f"{amount} ГРН - це приблизно {usd_result:.2f} USD")
+    print("Hello, World! Це моя перша практична робота з Git.")
+
+    amount = 1000
+    usd_result = uah_to_usd(amount)
+    print(f"{amount} ГРН - це приблизно {usd_result:.2f} USD")
